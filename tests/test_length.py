a3d46114de99de95181cf5cd8368c8ce05770f41
Siddharth72 <siddharth.navaneethan@gmail.com>
2026-07-21 19:47:50 +0530

    Update test_length.py

--- tests/test_length.py
+++ tests/test_length.py
@@ -1,1 +1,30 @@
+import unittest
 
+from length import convert_length
+
+
+class TestLengthConversion(unittest.TestCase):
+
+    def test_meter_to_feet(self):
+        result, unit = convert_length(
+            1,
+            "meters",
+            "feet"
+        )
+
+        self.assertAlmostEqual(result, 3.28084)
+        self.assertEqual(unit, "feet")
+
+    def test_feet_to_meter(self):
+        result, unit = convert_length(
+            3.28084,
+            "feet",
+            "meters"
+        )
+
+        self.assertAlmostEqual(result, 1)
+        self.assertEqual(unit, "meters")
+
+
+if __name__ == "__main__":
+    unittest.main()
